fbdd1a2450ec794e3a961301f58d193f27cb934e
Willian C. Klein <willian.empari@gmail.com>
2016-12-16 09:08:07 -0200

    change redirect url

--- src/config/pagseguro.php
+++ src/config/pagseguro.php
@@ -3,5 +3,5 @@
 return [
   'email' => env('PAGSEGURO_EMAIL', 'email@frompagseguro.com'),
   'token' => env('PAGSEGURO_TOKEN', 'tokenpagseguro'),
-  'redirect_url' => env('PAGSEGURO_REDIRECT_URL','https://licita.plus/')
+  'redirect_url' => env('PAGSEGURO_REDIRECT_URL','https://meusite.plus/')
 ];
